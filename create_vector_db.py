b2be52137ed824b80f5092c15845106bf9396c1b
KillianLucas <63927363+KillianLucas@users.noreply.github.com>
2023-08-08 18:38:38 +0000

    Added support for trigger phrases

--- create_vector_db.py
+++ create_vector_db.py
@@ -1,31 +1,59 @@
-import openai
 import os
+import re
 import json
+import openai
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-# Path to the folder containing the text files
 folder_path = 'procedures'
 
-# Read the procedures from the text files
-strings = []
-for filename in os.listdir(folder_path):
+# Structures to hold embeddings and texts
+embeddings_data = []
+texts_data = {}
+
+# Iterate through files
+for idx, filename in enumerate(os.listdir(folder_path)):
     if filename.endswith(".txt"):
         with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as file:
-            procedure = file.read().strip()
-            strings.append(procedure)
+            # Read the content from the file
+            content = file.read().strip()
+            
+            # Check if "trigger phrases:" is present in the content
+            if content.startswith("trigger phrases:"):
+                # Use regular expressions to find all phrases enclosed in quotes after "trigger phrases:"
+                key_phrases_line = content.split("\n")[0]
+                key_phrases = re.findall(r'"(.*?)"', key_phrases_line)
+            
+                # Extract the full text by taking everything after the first line
+                full_text = "\n".join(content.split("\n")[1:]).strip()
+            else:
+                # If "trigger phrases:" is not present, consider the entire content as the full text
+                key_phrases = []
+                full_text = content
 
-# Generate embeddings for the list of strings using OpenAI Ada
-embeddings = []
-for string in strings:
-    response = openai.Embedding.create(
-        input=string,
-        model="text-embedding-ada-002"
-    )
-    embeddings.append(response['data'][0]['embedding'])
+            # Store full text
+            texts_data[str(idx)] = full_text
 
-# Save the embeddings and the list of strings to a JSON file
+            # Embed full text
+            response = openai.Embedding.create(
+                input=full_text,
+                model="text-embedding-ada-002"
+            )
+            embeddings_data.append({"id": idx, "embedding": response['data'][0]['embedding']})
+
+            # Embed key phrases
+            for phrase in key_phrases:
+                response = openai.Embedding.create(
+                    input=phrase,
+                    model="text-embedding-ada-002"
+                )
+                embeddings_data.append({"id": idx, "embedding": response['data'][0]['embedding']})
+
+# Save embeddings and texts
 with open('vector_db.json', 'w') as f:
-    json.dump({'strings': strings, 'embeddings': embeddings}, f)
+    json.dump({"embeddings": embeddings_data}, f)
 
-print("Embeddings and procedures saved to vector_db.json")
+with open('text_db.json', 'w') as f:
+    json.dump({"texts": texts_data}, f)
+
+print("Embeddings and texts saved.")
--- main.py
+++ main.py
@@ -10,14 +10,16 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-app = FastAPI()
-
-# Load the saved procedures and embeddings
+# Load the saved embeddings
 with open('vector_db.json', 'r') as f:
-  data = json.load(f)
-  strings = data['strings']
-  embeddings = data['embeddings']
+  embedding_data = json.load(f)
+  embeddings = [item['embedding'] for item in embedding_data['embeddings']]
 
+# Load the saved texts
+with open('text_db.json', 'r') as f:
+  texts = json.load(f)['texts']
+
+app = FastAPI()
 
 @app.get("/search/")
 async def search_procedure(query: str):
@@ -25,15 +27,23 @@ async def search_procedure(query: str):
   response = openai.Embedding.create(input=query,
                                      model="text-embedding-ada-002")
   query_embedding = response['data'][0]['embedding']
-
-  # Compute cosine similarity between the query embedding and the saved embeddings
+  
+  # Compute cosine similarity
   similarities = cosine_similarity([query_embedding], embeddings)[0]
 
   # Get the indices of the embeddings sorted by similarity
   sorted_indices = similarities.argsort()[::-1]
 
-  # Get the top 2 most similar procedures
-  top_procedures = [strings[index] for index in sorted_indices[:2]]
+  # Collect unique top procedure IDs
+  top_ids = set()
+  top_procedures = []
+  for index in sorted_indices:
+    id_ = embedding_data['embeddings'][index]['id']
+    if id_ not in top_ids:
+      top_procedures.append(texts[str(id_)])
+      top_ids.add(id_)
+      if len(top_procedures) == 2: # Change this value to control the number of results
+        break
 
   # Return the corresponding procedures
   return {"procedures": top_procedures}
@@ -44,12 +54,10 @@ async def home():
   # Convert README.md into HTML
   with open('README.md', 'r') as file:
     content = file.read()
-    print("c", content)
 
   # Remove ```python and ```bash language tags, which aren't properly converted
   content = content.replace("```python", "```").replace("```bash", "```")
   content = markdown.markdown(content)
-  print("co", content)
 
   # Replace <p><code> with <pre><code> and </code></p> with </code></pre>
   content = re.sub(r'<p><code>(.*?)\n', r'<pre><code>\1\n', content)
